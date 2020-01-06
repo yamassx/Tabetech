d5f2bc5a9e00fbd5b2ba396b8707e5a4c71bf8a4
yamassx <58455442+yamassx@users.noreply.github.com>
2020-01-06 16:47:59 +0900

    スライドショーの実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,18 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+
 //= require_tree .
+$(function() {
+  $('.landing_page__contents__image_slicker').not('.slick-initialized').slick({
+    autoplay: true,
+    fade: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+  });
+});
